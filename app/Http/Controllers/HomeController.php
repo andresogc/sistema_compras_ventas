a59e9b736bf2e09cb57949614c1b9460c5a30bd6
andresogc <andresogc2011@gmail.com>
2020-03-05 16:03:43 -0500

    Se agrega graficos de compras

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
-    }
+
+        $comprasmes=DB::select('SELECT monthname(c.fecha_compra) as mes, sum(c.total) as totalmes from compras c where c.estado="Registrado" group by monthname(c.fecha_compra) order by month(c.fecha_compra) desc limit 12');
+
+        $ventasmes=DB::select('SELECT monthname(v.fecha_venta) as mes, sum(v.total) as totalmes from ventas v where v.estado="Registrado" group by monthname(v.fecha_venta) order by month(v.fecha_venta) desc limit 12');
+
+        $ventasdia=DB::select('SELECT DATE_FORMAT(v.fecha_venta,"%d/%m/%Y") as dia, sum(v.total) as totaldia from ventas v where v.estado="Registrado" group by v.fecha_venta order by day(v.fecha_venta) desc limit 15');
+
+        $productosvendidos=DB::select('SELECT p.nombre as producto, sum(dv.cantidad) as cantidad from productos p inner join detalle_ventas dv on p.id=dv.idproducto inner join ventas v on dv.idventa=v.id where v.estado="Registrado" and year(v.fecha_venta)=year(curdate()) group by p.nombre order by sum(dv.cantidad) desc limit 10');
+
+        $totales=DB::select('SELECT (select ifnull(sum(c.total),0) from compras c where DATE(c.fecha_compra)=curdate() and c.estado="Registrado") as totalcompra, (select ifnull(sum(v.total),0) from ventas v where DATE(v.fecha_venta)=curdate() and v.estado="Registrado") as totalventa');
+
+            return view('home',["comprasmes"=>$comprasmes,"ventasmes"=>$ventasmes,"ventasdia"=>$ventasdia,"productosvendidos"=>$productosvendidos,"totales"=>$totales]);
+
+        }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,198 @@
 @extends('principal')
-
 @section('contenido')
 <main class="main">
-    <!-- Breadcrumb -->
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item active"><a href="/">BACKEND - SISTEMA DE COMPRAS - VENTAS</a></li>
-    </ol>
-    <div class="container-fluid">
-        HOME
-    </div>
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="/">BACKEND - SISTEMA DE COMPRAS - VENTAS</a></li>
+            </ol>
+            <div class="container-fluid">
+
+
+                @foreach($totales as $total)
+
+                <div class="row">
+
+                            <div class="col-lg-6 col-xs-6">
+                            <!-- small box -->
+                            <div class="card text-white bg-success">
+                                <div class="card-body pb-0">
+                                    <button class="btn btn-transparent p-0 float-right" type="button">
+                                    <i class="fa fa-shopping-cart fa-4x"></i>
+                                    </button>
+                                    <div class="text-value h2"><strong>USD {{$total->totalcompra}} (MES ACTUAL)</strong></div>
+                                    <div class="h2">Compras</div>
+                                </div>
+                                <div class="chart-wrapper mt-3 mx-3" style="height:35px;">
+                                    <a href="{{url('compra')}}" class="small-box-footer h4">Compras <i class="fa fa-arrow-circle-right"></i></a>
+                                </div>
+
+                            </div>
+                            </div>
+
+                            <div class="col-lg-6 col-xs-6">
+                            <!-- small box -->
+                            <div class="card text-white bg-warning">
+                                <div class="card-body pb-0">
+                                    <button class="btn btn-transparent p-0 float-right" type="button">
+                                    <i class="fa fa-suitcase fa-4x"></i>
+                                    </button>
+                                    <div class="text-value h2"><strong>USD {{$total->totalventa}} (MES ACTUAL) </strong></div>
+                                    <div class="h2">Ventas</div>
+                                </div>
+                                <div class="chart-wrapper mt-3 mx-3" style="height:35px;">
+                                    <a href="{{url('venta')}}" class="small-box-footer h4">Ventas <i class="fa fa-arrow-circle-right"></i></a>
+                                </div>
+
+                            </div>
+
+
+                            </div><!-- ./col -->
+
+                        </div>
+
+                @endforeach
+
+
+                        <!-- Estadísticas gráficos -->
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- compras - meses -->
+
+                                <div class="card card-chart">
+                                    <div class="card-header">
+                                        <h4 class="text-center">Compras - Meses</h4>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="ct-chart">
+                                            <canvas id="compras">
+                                            </canvas>
+                                        </div>
+                                    </div>
+
+                            </div>
+
+                            </div><!--col-md-6-->
+
+                            <div class="col-md-6">
+
+                                <!-- ventas - meses -->
+
+                                <div class="card card-chart">
+                                    <div class="card-header">
+                                        <h4 class="text-center">Ventas - Meses</h4>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="ct-chart">
+                                            <canvas id="ventas">
+                                            </canvas>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+                            </div><!-- col-md-6 -->
+
+                        </div><!--row-->
 
 
 
-</main>
+
+                @push ('scripts')
+                <script src="{{asset('js/Chart.min.js')}}"></script>
+
+                    <script>
+                    $(function () {
+                        /* ChartJS
+                        * -------
+                        * Here we will create a few charts using ChartJS
+                        */
+
+                        //--------------
+                        //- AREA CHART -
+                        //--------------
+
+                        /**inicio de compras mes */
+
+                        var varCompra=document.getElementById('compras').getContext('2d');
+
+                            var charCompra = new Chart(varCompra, {
+                                type: 'line',
+                                data: {
+                                    labels: [<?php foreach ($comprasmes as $reg)
+                                        {
+
+                                    setlocale(LC_TIME, 'es_ES', 'Spanish_Spain', 'Spanish');
+                                    $mes_traducido=strftime('%B',strtotime($reg->mes));
+
+                                    echo '"'. $mes_traducido.'",';} ?>],
+                                    datasets: [{
+                                        label: 'Compras',
+                                        data: [<?php foreach ($comprasmes as $reg)
+                                            {echo ''. $reg->totalmes.',';} ?>],
+
+                                        borderColor: 'rgba(255, 99, 132, 1)',
+                                        borderWidth:3
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }]
+                                    }
+                                }
+                            });
+
+                            /*fin compras mes* */
+
+
+                        /**inicio de ventas mes */
+                        var varVenta=document.getElementById('ventas').getContext('2d');
+
+                            var charVenta = new Chart(varVenta, {
+                                type: 'bar',
+                                data: {
+                                    labels: [<?php foreach ($ventasmes as $reg)
+                                {
+                                    setlocale(LC_TIME, 'es_ES', 'Spanish_Spain', 'Spanish');
+                                    $mes_traducido=strftime('%B',strtotime($reg->mes));
+
+                                    echo '"'. $mes_traducido.'",';} ?>],
+                                    datasets: [{
+                                        label: 'Ventas',
+                                        data: [<?php foreach ($ventasmes as $reg)
+                                        {echo ''. $reg->totalmes.',';} ?>],
+                                        backgroundColor: 'rgba(20, 204, 20, 1)',
+                                        borderColor: 'rgba(54, 162, 235, 0.2)',
+                                        borderWidth: 1
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }]
+                                    }
+                                }
+                            });
+
+                    });
+
+                    /*fin ventas mes* */
+
+
+
+                    </script>
+                @endpush
+
+            </div>
+
+
+        </main>
+
 @endsection
-
